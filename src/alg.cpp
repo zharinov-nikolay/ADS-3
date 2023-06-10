f81db3281f24f18df3fcf967e79fd42d77a80562
zharinov-nikolay <125186380+zharinov-nikolay@users.noreply.github.com>
2023-06-10 12:06:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,28 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int N = 0;
+    int K = size - 1;
+    int kol = 0;
+    for (int i = 0; i < K; i++) {
+        int C = N + (K - N) / 2;
+        if (arr[C] == value) {
+            if (arr[C - 1] == value || arr[C + 1] == value) {
+                for (int j = C; arr[j] == value; j--) {
+                    kol += 1;
+                }
+                for (int p = C + 1; arr[p] == value; p++) {
+                    kol += 1;
+                }
+                return kol;
+            } else {
+                return 1;
+            }
+        } else if (arr[C] < value) {
+            N = C + 1;
+        } else {
+            K = C;
+        }
+    }
+    return 0;
 }
